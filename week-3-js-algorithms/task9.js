adddf2bad29bd9e75e5a815cbed2d55156ce8287
Aleksandr <aik5787@gmail.com>
2024-06-12 22:55:40 -0400

    update

--- week-3-js-algorithms/task9.js
+++ week-3-js-algorithms/task9.js
@@ -1,9 +1,14 @@
 function compareAge(name, age) {
-    if(age>25){
+    const myAge = 25;
+    if(age>myAge){
         return `${name} is older than me`
     }
-    else {
+    else if (age<myAge) {
         return `${name} is younger than me`
+
+    }
+    else {
+        return `${name}  has the same age`
     }
 }
 
@@ -13,3 +18,4 @@ function compareAge(name, age) {
 console.log(compareAge("Joel", 36)); // ➞ "Joel is older than me."
 console.log(compareAge("Samuel", 24)); // ➞ "Samuel is younger than me."
 console.log(compareAge("Lily", 28)); // ➞ "Lily is older than me."
+console.log(compareAge("Jolly", 25)); 
