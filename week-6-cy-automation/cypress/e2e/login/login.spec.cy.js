1fd48ecfd7248794d06e9c5bcc6ef1c5a2ce5a09
Aleksandr <aik5787@gmail.com>
2024-07-04 14:47:14 -0400

    week-6-cy update

--- week-6-cy-automation/cypress/e2e/login/login.spec.cy.js
+++ week-6-cy-automation/cypress/e2e/login/login.spec.cy.js
@@ -2,23 +2,21 @@ describe("Login", () => {
   beforeEach(() => {
     cy.visit("/");
     cy.get('[href="/auth/login"]').click();
-    cy.get('[name="email"]').type('test12345@gmail.com');
-    cy.get('[name="password"]').type('qwerty123');
-    cy.contains('Login').click();
+    cy.get('[name="email"]').type("test12345@gmail.com");
+    cy.get('[name="password"]').type("qwerty123");
+    cy.contains("Login").click();
   });
 
   it("Log in successfully!", () => {
-   
-    cy.get('a p').should('have.text', 'role: user');
-    cy.title().should('eq', 'User: Profile | Delek Homes');
-
+    cy.get("a p").should("have.text", "role: user");
+    cy.title().should("eq", "User: Profile | Delek Homes");
   });
 
-   it("Log out successfully!", () => {
+  it("Log out successfully!", () => {
     cy.get('button [data-testid="PersonIcon"]').click();
-    cy.contains('Logout').click();
+    cy.contains("Logout").click();
 
-    cy.url().should('include', 'auth/login')
-    cy.title().should('eq', 'Login | Delek Homes');
+    cy.url().should("include", "auth/login");
+    cy.title().should("eq", "Login | Delek Homes");
   });
 });
--- week-6-cy-automation/cypress/e2e/registration/registratin.spec.cy.js
+++ week-6-cy-automation/cypress/e2e/registration/registratin.spec.cy.js
@@ -1,4 +1,4 @@
-import { faker } from '@faker-js/faker';
+import { faker } from "@faker-js/faker";
 
 const email = faker.internet.email();
 const password = faker.internet.password();
@@ -19,9 +19,8 @@ describe("Registration", () => {
     cy.get("a p").should("have.text", "role: user");
     cy.get("h6").should("have.text", "Test  Testerov");
     cy.title().should("eq", "User: Profile | Delek Homes");
-    cy.url().should('include', 'dashboard/user/profile')
+    cy.url().should("include", "dashboard/user/profile");
   });
-
 
   it("Should not register with an already existing email account", () => {
     cy.get('[href="/auth/register"]').click();
@@ -31,20 +30,28 @@ describe("Registration", () => {
     cy.get('[name="password"]').type(password);
     cy.get('[type="submit"]').click();
 
-    cy.get("form .MuiAlert-message").should("exist").and("have.text", "Input data validation failed");
-    cy.url().should('include', 'auth/register')
+    cy.get("form .MuiAlert-message")
+      .should("exist")
+      .and("have.text", "Input data validation failed");
+    cy.url().should("include", "auth/register");
   });
-
 
   it("Should not register without filling in required fields", () => {
     cy.get('[href="/auth/register"]').click();
     cy.get('[type="submit"]').click();
 
-    cy.get('[id=":r5:-helper-text"]').should("exist").and("have.text", "First name required");
-    cy.get('[id=":r6:-helper-text"]').should("exist").and("have.text", "Last name required");
-    cy.get('[id=":r7:-helper-text"]').should("exist").and("have.text", "Email is required");
-    cy.get('[id=":r8:-helper-text"]').should("exist").and("have.text", "Password is required");
-    cy.url().should('include', 'auth/register')
+    cy.get('[id=":r5:-helper-text"]')
+      .should("exist")
+      .and("have.text", "First name required");
+    cy.get('[id=":r6:-helper-text"]')
+      .should("exist")
+      .and("have.text", "Last name required");
+    cy.get('[id=":r7:-helper-text"]')
+      .should("exist")
+      .and("have.text", "Email is required");
+    cy.get('[id=":r8:-helper-text"]')
+      .should("exist")
+      .and("have.text", "Password is required");
+    cy.url().should("include", "auth/register");
   });
-
 });
